5b5a30c7afca866689d6ca842e5fafd34f4af9e1
Pratik <kpratik2238@gmail.com>
2022-09-15 19:19:11 +0530

    this is feature 3rd commit

--- MethodCalling.java
+++ MethodCalling.java
@@ -2,15 +2,41 @@ package InClassCodes;
 
 public class MethodCalling {
 
-	public static int result() {
-		int number = 10 + ((50 * 2) * 45) + 5 + (58 * 3);
-		return number;
+	public static int multiplication1() {
+		int num1 = 50;
+		int num2 = 2;
+		int num3 = 45;
+
+		int mulresult = num1 * num2 * num3;
+		return mulresult;
+
+	}
+
+	public static int multiplication2() {
+		int num1 = 58;
+		int num2 = 3;
+
+		int mulresult2 = num1 * num2;
+		return mulresult2;
+
+	}
+
+	public static int addition() {
+		int num1 = 10;
+		int num2 = 5;
+		int add = num1 + num2;
+		return add;
 	}
 
 	public static void main(String[] args) {
 
-		int result = MethodCalling.result();
-		System.out.println(result);
+		int result1 = MethodCalling.multiplication1();
+		int result2 = MethodCalling.multiplication2();
+		int result3 = MethodCalling.addition();
+
+		int finalresult = result1 + result2 + result3;
+		System.out.println(finalresult);
+
 	}
 
 }
